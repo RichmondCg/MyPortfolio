ef76ec8207eb1b9c1a39d231931825e9cf4331b5
richmondgillaco <richmondgillaco@richmondgillacos-MacBook-Air.local>
2026-04-03 22:05:29 +0800

    Footer fix

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className="flex flex-col lg:flex-row justify-between mx-7 lg:mx-20 my-4 space-y-4 lg:space-y-0 text-base text-[#1B263B]">
       <ul className="flex gap-12">
         <li>
-          <a href="/MyPortfolio">Home</a>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <a href="/MyPortfolio/about">About</a>
+          <Link to="/about">About</Link>
         </li>
         <li>
-          <a href="/MyPortfolio/projects">Projects</a>
+          <Link to="/projects">Projects</Link>
         </li>
       </ul>
       <p>&copy; 2026 Richmond Gillaco. All rights reserved.</p>
